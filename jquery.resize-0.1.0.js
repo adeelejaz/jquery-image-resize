db167fab0c375a373d2a2d46e3a7a10accd9a714
Adeel Ejaz <adeel.e@gmail.com>
2010-06-17 00:06:29 +0100

    Initial commit

---
+++ jquery.resize-0.1.0.js
@@ -0,0 +1,25 @@
+jQuery.fn.resize = function(options) {
+	var params = jQuery.extend({
+		height: 9,
+		width: 9
+	}, options);
+
+	this.each(function() {
+		var height = params.height,
+			width = params.width,
+			img_height = $(this).height(),
+			img_width = $(this).width(),
+			m_ceil = Math.ceil,
+			m_floor = Math.floor;
+
+		if (img_height >= img_width) {
+			width = m_floor(m_ceil(img_width / img_height * height));
+		} else {
+			height = m_floor(m_ceil(img_height / img_width * width));
+		}
+		$(this).attr({
+			'height': height,
+			'width': width
+		});
+	});
+};
