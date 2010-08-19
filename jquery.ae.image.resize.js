8883a65bc7f70937d1ba4efea55b5f6d6fde3e5e
Adeel Ejaz <adeel.e@gmail.com>
2010-08-19 22:27:24 +0100

    Fix for webkit browsers where no width or height is specified

--- jquery.ae.image.resize.js
+++ jquery.ae.image.resize.js
@@ -7,23 +7,30 @@
 				width: 9
 			}, options);
 
+		// Loop through each
 		this.each(function() {
+
+			// Bind to load event
+			// Fixes Webkit browsers as they set
+			// height and width after image is loaded
+			$(this).load(function() {
 			var imgHeight = $(this).height(),
 				imgWidth = $(this).width(),
 				height = params.height,
 				width = params.width;
 
-			if (imgHeight > height || imgWidth > width) {
-				if (imgHeight > imgWidth)
-					width = mathFloor(mathCeil(imgWidth / imgHeight * height));
-				else
-					height = mathFloor(mathCeil(imgHeight / imgWidth * width));
-				
-				$(this).attr({
-					'height': height,
-					'width': width
-				});
-			}
+				if (imgHeight > height || imgWidth > width) {
+					if (imgHeight > imgWidth)
+						width = mathFloor(mathCeil(imgWidth / imgHeight * height));
+					else
+						height = mathFloor(mathCeil(imgHeight / imgWidth * width));
+
+					$(this).attr({
+						'height': height,
+						'width': width
+					});
+				}
+			});
 		});
 	};
 })(jQuery);
