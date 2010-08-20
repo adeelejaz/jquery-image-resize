7fe11f2b92ea1d2e855b54befa3a4a77b4b371a1
Adeel Ejaz <adeel.e@gmail.com>
2010-08-20 19:54:35 +0100

    Trigger load event for Firefox and IE

--- jquery.ae.image.resize.js
+++ jquery.ae.image.resize.js
@@ -4,38 +4,44 @@
 			params = jQuery.extend({
 				height: 9,
 				width: 9
-			}, options);
+			}, options),
+			isIE6 = $.browser.msie && (parseInt($.browser.version) == 6);
 
-		// Loop through each
-		this.each(function() {
+		// Attach handler to load
+		// Handler is executed just once per element
+		// Load event required to fixes Webkit browsers
+		$(this).one('load', function() {
 
-			// Bind to load event
-			// Fixes Webkit browsers as they set
-			// height and width after image is loaded
-			$(this).load(function() {
+			// Remove all attributes and CSS rules
+			$(this).removeAttr('height')
+				   .removeAttr('width')
+				   .css({height: '', width: ''});
 
-				// Remove all attributes and CSS rules
-				$(this).removeAttr('height')
-					   .removeAttr('width')
-					   .css({ height: '', width: ''});
+			var imgHeight = this.height,
+				imgWidth = this.width,
+				height = params.height,
+				width = params.width;
 
-				var imgHeight = this.height,
-					imgWidth = this.width,
-					height = params.height,
-					width = params.width;
-
-				if (imgHeight > height || imgWidth > width) {
-					if (imgHeight > imgWidth)
-						width = mathFloor(imgWidth / imgHeight * height);
-					else
-						height = mathFloor(imgHeight / imgWidth * width);
-
-					$(this).attr({
-						'height': height,
-						'width': width
-					});
+			if (imgHeight > height || imgWidth > width) {
+				if (imgHeight > imgWidth) {
+					width = mathFloor(imgWidth / imgHeight * height);
+				} else {
+					height = mathFloor(imgHeight / imgWidth * width);
 				}
-			});
+
+				$(this).attr({
+					'height': height,
+					'width': width
+				});
+			}
+		})
+		.each(function() {
+
+			// Trigger load event (for Gecko and MSIE)
+			if (this.complete || isIE6)
+			{
+				$(this).trigger('load');
+			}
 		});
 	};
 })(jQuery);
