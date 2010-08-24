b17cf85406a0f2dbaaaa0c4efaa48b4b75f54dd0
Adeel Ejaz <adeel.e@gmail.com>
2010-08-24 21:30:30 +0100

    Typo

--- jquery.ae.image.resize.js
+++ jquery.ae.image.resize.js
@@ -9,7 +9,7 @@
 
 		// Attach handler to load
 		// Handler is executed just once per element
-		// Load event required to fixes Webkit browsers
+		// Load event required to fixe Webkit browsers
 		$(this).one('load', function() {
 
 			// Remove all attributes and CSS rules
