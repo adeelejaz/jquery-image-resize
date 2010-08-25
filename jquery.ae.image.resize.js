2ad258536b288bebf42ee6b941e316d7e83e1f79
Adeel Ejaz <adeel.e@gmail.com>
2010-08-25 01:26:41 +0100

    Use aspect ratio of the bounding box as basis to resize, plus more optimization for minification

--- jquery.ae.image.resize.js
+++ jquery.ae.image.resize.js
@@ -1,11 +1,15 @@
 (function($) {
 	$.fn.aeImageResize = function(options) {
-		var mathFloor = Math.floor,
-			params = jQuery.extend({
+		var propertyHeight = 'height',
+			propertyWidth = 'width',
+			browser = $.browser,
+			mathFloor = Math.floor,
+			params = $.extend({
 				height: 9,
 				width: 9
 			}, options),
-			isIE6 = $.browser.msie && (parseInt($.browser.version) == 6);
+			isIE6 = browser.msie && (parseInt(browser.version) == 6),
+			aspectRatio = params[propertyWidth] / params[propertyHeight];
 
 		// Attach handler to load
 		// Handler is executed just once per element
@@ -13,20 +17,24 @@
 		$(this).one('load', function() {
 
 			// Remove all attributes and CSS rules
-			$(this).removeAttr('height')
-				   .removeAttr('width')
+			$(this).removeAttr(propertyHeight)
+				   .removeAttr(propertyWidth)
 				   .css({height: '', width: ''});
 
-			var imgHeight = this.height,
-				imgWidth = this.width,
-				height = params.height,
-				width = params.width;
+			var imgHeight = this[propertyHeight],
+				imgWidth = this[propertyWidth],
+				height = params[propertyHeight],
+				width = params[propertyWidth],
+				imgAspectRatio = imgWidth / imgHeight;
 
+			// Only resize the images that are bigger
+			// than the bounding box
 			if (imgHeight > height || imgWidth > width) {
-				if (imgHeight > imgWidth) {
-					width = mathFloor(imgWidth / imgHeight * height);
-				} else {
+
+				if (imgAspectRatio > aspectRatio) {
 					height = mathFloor(imgHeight / imgWidth * width);
+				} else {
+					width = mathFloor(imgWidth / imgHeight * height);
 				}
 
 				$(this).attr({
