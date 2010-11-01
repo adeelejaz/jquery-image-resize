5b56c301e2703e5ea5bd5c1f8b93bb4b7fb18b41
Adeel Ejaz <adeel.e@gmail.com>
2010-11-01 00:33:27 +0000

    Some clean up

--- jquery.ae.image.resize.js
+++ jquery.ae.image.resize.js
@@ -1,15 +1,18 @@
 (function($) {
-	$.fn.aeImageResize = function(options) {
-		var strHeight = 'height',
-			strWidth = 'width',
-			browser = $.browser,
-			mathFloor = Math.floor,
-			params = $.extend({
+
+	$.fn.aeImageResize = function( options ) {
+
+		var strHeight = "height"
+		,	strWidth = "width"
+		,	mathFloor = Math.floor
+		,	browser = $.browser
+		,	isIE6 = browser.msie && (parseInt(browser.version) == 6)
+		,	aspectRatio = 0
+		,	params = $.extend({
 				height: 0,
 				width: 0
-			}, options),
-			isIE6 = browser.msie && (parseInt(browser.version) == 6),
-			aspectRatio = params[strWidth] / params[strHeight];
+			}, options)
+		;
 
 		// We cannot do much unless we have one of these
 		if ( !params[strHeight] && !params[strWidth] ) {
@@ -24,24 +27,27 @@
 		// Attach handler to load
 		// Handler is executed just once per element
 		// Load event required for Webkit browsers
-		$(this).one('load', function() {
+		$( this ).one( "load", function() {
 
 			// Remove all attributes and CSS rules
-			$(this).removeAttr(strHeight)
-				   .removeAttr(strWidth)
-				   .css({height: '', width: ''});
+			$( this ).removeAttr( strHeight )
+					 .removeAttr( strWidth )
+					 .css({
+						 height: '',
+						 width: ''
+					 });
 
-			var imgHeight = this[strHeight],
-				imgWidth = this[strWidth],
-				imgAspectRatio = imgWidth / imgHeight,
-				bxHeight = params[strHeight],
-				bxWidth = params[strWidth],
-				bxAspectRatio = aspectRatio;
+			var imgHeight = this[strHeight]
+			,	imgWidth = this[strWidth]
+			,	imgAspectRatio = imgWidth / imgHeight
+			,	bxHeight = params[strHeight]
+			,	bxWidth = params[strWidth]
+			,	bxAspectRatio = aspectRatio;
 				
 			// Work the magic!
 			// If one parameter is missing, we just force calculate it
 			if ( !bxAspectRatio ) {
-				if (bxHeight === 0) {
+				if ( bxHeight === 0 ) {
 					bxAspectRatio = imgAspectRatio - 1;
 				} else {
 					bxAspectRatio = imgAspectRatio + 1;
@@ -51,24 +57,23 @@
 			// Only resize the images that need resizing
 			if ( (bxHeight && imgHeight > bxHeight) || (bxWidth && imgWidth > bxWidth) ) {
 
-				if (imgAspectRatio > aspectRatio) {
-					bxHeight = mathFloor(imgHeight / imgWidth * bxWidth);
+				if ( imgAspectRatio > aspectRatio ) {
+					bxHeight = mathFloor( imgHeight / imgWidth * bxWidth );
 				} else {
-					bxWidth = mathFloor(imgWidth / imgHeight * bxHeight);
+					bxWidth = mathFloor( imgWidth / imgHeight * bxHeight );
 				}
 
-				$(this).attr({
-					'height': bxHeight,
-					'width': bxWidth
+				$( this ).attr({
+					"height": bxHeight,
+					"width": bxWidth
 				});
 			}
 		})
 		.each(function() {
 
 			// Trigger load event (for Gecko and MSIE)
-			if (this.complete || isIE6)
-			{
-				$(this).trigger('load');
+			if ( this.complete || isIE6 ) {
+				$( this ).trigger( "load" );
 			}
 		});
 	};
